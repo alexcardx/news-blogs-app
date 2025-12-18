8121d202b919f96678e5fc898c5c1dd49a0512f4
alexcardx <alex.miocard@gmail.com>
2025-12-18 18:21:36 +0300

    fixed api

---
+++ api/news.js
@@ -0,0 +1,13 @@
+export default async function handler(
+  req,
+  res
+) {
+  const response = await fetch(
+    'https://gnews.io/api/v4/top-headlines?category=general&lang=en&apikey=' +
+      process.env.GNEWS_API_KEY
+  )
+
+  const data = await response.json()
+
+  res.status(200).json(data)
+}
